3e356fd0afa8621896884921c891411826748e83
Macielv7 <macielvinicius500@gmail.com>
2025-11-20 19:40:52 -0300

    first commit

---
+++ Controllers/TarefaController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Tarefas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TarefaController : ControllerBase
+    {
+        
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_Tarefas.Data
+{
+    public class AppDbContext
+    {
+        
+    }
+}
