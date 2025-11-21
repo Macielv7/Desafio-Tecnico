d980806a0a6fe5ad97fa35736b9e71bb091a575b
Macielv7 <macielvinicius500@gmail.com>
2025-11-20 22:14:25 -0300

    add model

--- Model/Tarefa.cs
+++ Model/Tarefa.cs
@@ -1,12 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace WebApi_Tarefas.Model
 {
     public class Tarefa
     {
-        
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O título não pode exceder 50 caracteres.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "A Descricao é obrigatório.")]
+        [MaxLength(100, ErrorMessage = "A Descricao não pode exceder 100 caracteres.")]
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public bool Concluida { get; set; }
     }
 }
